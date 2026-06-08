9f6ee5ef1feb020303037af08cee40928e13c7e8
Shaurya Dev Aggarwal <Shauryadev770@gmail.com>
2026-06-08 21:21:37 +0530

    Added Gambler's ruin

---
+++ Gambler's_ruin.py
@@ -0,0 +1,40 @@
+'''
+Lets say there are 10,000 people playing a gambling game where the chances of winning are 50%. Lets find out the 
+probability of going broke at the end.
+'''
+
+import random
+
+Gamble = ["Win","Loss"]
+
+def gamble():
+    capital = 100
+    while True:
+        
+        final = ()
+        r = random.choice(Gamble)
+        if r == "Win":
+            capital+=1
+        if r == "Loss":
+            capital-=1
+                                          
+        if capital ==0 or capital == 200:
+            break
+        else:
+            continue
+    final = capital
+    return final
+
+n = 0
+total =[]
+while n<=9999:
+    total.append(gamble())   
+    n+=1
+
+print(total)
+broke = total.count(0)
+
+probability_broke = (broke/10000)*100
+print("Probability of going broke: ",probability_broke,"%")
+
+
