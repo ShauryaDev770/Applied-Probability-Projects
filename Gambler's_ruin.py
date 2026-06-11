72ba018a401b415fdea6093f398f272d929b7379
Shaurya Dev Aggarwal <Shauryadev770@gmail.com>
2026-06-11 16:05:06 +0530

    Updated Gambler's ruin

--- Gambler's_ruin.py
+++ Gambler's_ruin.py
@@ -1,21 +1,20 @@
 '''
 Lets say there are 10,000 people playing a gambling game where upon winning, the player gets 10 ruppes. Lets find out the 
 probability of going broke at the end.
+But there is a twist, the game is a little biased by 0.01% against them.
 '''
 
 import random
-
-Gamble = ["Win","Loss"]
 
 def gamble():
     capital = 100
     while True:
         
         final = ()
-        r = random.choice(Gamble)
-        if r == "Win":
+        r_boolean = random.random() < 0.49
+        if r_boolean == True:
             capital+=10
-        if r == "Loss":
+        if r_boolean == False:
             capital-=10
                                           
         if capital ==0 or capital == 200:
